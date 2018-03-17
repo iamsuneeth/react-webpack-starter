5d6c32e6bb2b9dc99e02de87715667b1c2a351b7
Suneeth <suneeth.lenin@gmail.com>
2018-03-17 16:33:47 +0530

    added all the basic configurations

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: 'index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'components.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader','eslint-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/, use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { modules: true } },
+                    'postcss-loader'
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
